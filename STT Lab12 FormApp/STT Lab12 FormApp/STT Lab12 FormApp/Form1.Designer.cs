966c34da2ff9559f7c0f0978f650a64d418ac297
chira603 <chiragkumar.patel@iitgn.ac.in>
2025-04-22 16:17:09 +0530

    Updated

---
+++ STT Lab12 FormApp/STT Lab12 FormApp/STT Lab12 FormApp/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace STT_Lab12_FormApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label title;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // 
+            // title
+            // 
+            this.title.Font = new System.Drawing.Font("Times New Roman", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.title.ForeColor = System.Drawing.Color.DarkBlue;
+            this.title.Location = new System.Drawing.Point(0, 10);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(450, 50);
+            this.title.Text = "⏰ Alarm Clock";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPrompt.Location = new System.Drawing.Point(30, 80);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(110, 30);
+            this.lblPrompt.Text = "Set Time:";
+            this.lblPrompt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+
+            // 
+            // txtTime
+            // 
+            this.txtTime.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtTime.Location = new System.Drawing.Point(150, 80);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(180, 32);
+            this.txtTime.TabIndex = 0;
+            this.txtTime.PlaceholderText = "HH:mm:ss";
+
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnStart.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnStart.Location = new System.Drawing.Point(125, 130);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(200, 50);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start Alarm";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lblStatus.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblStatus.Location = new System.Drawing.Point(25, 200);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(400, 25);
+            this.lblStatus.Text = "";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(450, 270);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.lblPrompt);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form1";
+            this.Text = "Alarm Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ STT_lab_12/STT_lab_12/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+
+namespace TimeAlarmConsoleApp
+{
+    // 1. Define a delegate that matches the signature of the event handlers
+    public delegate void AlarmEventHandler(object sender, EventArgs args);
+
+    // 2. Publisher class that raises the alarm event
+    class AlarmClock
+    {
+        // The event to which subscribers can attach
+        public event AlarmEventHandler RaiseAlarm;
+
+        private readonly TimeSpan targetTime;
+
+        public AlarmClock(TimeSpan targetTime)
+        {
+            this.targetTime = targetTime;
+        }
+
+        // Method to start monitoring the time
+        public void Start()
+        {
+            Console.WriteLine($"Alarm set for {targetTime:hh\\:mm\\:ss}. Monitoring system time...");
+            while (true)
+            {
+                Console.WriteLine("Tick...");
+                TimeSpan now = DateTime.Now.TimeOfDay;
+                if (now.Hours == targetTime.Hours &&
+                    now.Minutes == targetTime.Minutes &&
+                    now.Seconds == targetTime.Seconds)
+                {
+                    OnRaiseAlarm();
+                    break;
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        // Raises the RaiseAlarm event
+        protected virtual void OnRaiseAlarm()
+        {
+            RaiseAlarm?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    class Program
+    {
+        // 3. Subscriber method that handles the alarm
+        static void RingAlarm(object sender, EventArgs args)
+        {
+            Console.WriteLine("*****  ALARM!  *****");
+            Console.WriteLine("The target time has been reached.");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter target time (HH:MM:SS): ");
+            string input = Console.ReadLine();
+
+            if (!TimeSpan.TryParseExact(input, "c", null, out TimeSpan target))
+            {
+                Console.WriteLine("Invalid format. Please use HH:MM:SS.");
+                return;
+            }
+
+            // 4. Create publisher and subscribe the RingAlarm handler
+            AlarmClock clock = new AlarmClock(target);
+            clock.RaiseAlarm += RingAlarm;
+
+            // 5. Start monitoring
+            clock.Start();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
